f9d2ed7c7511715303151049cca5667b83afd0f8
mohitrajrathor <mohitrajrathor@gmail.com>
2023-04-10 23:37:36 +0530

    handled  bhavcopy date error.

--- fatch_data.py
+++ fatch_data.py
@@ -2,17 +2,37 @@
 import requests
 import pandas as pd
 import numpy as np
+import datetime
+import re
 
+
+# exceptions class
+class BadDateFormat(Exception):
+    """ raised if date is not specified in correct or instructed format. """
+    pass
+
+class UnKnownErrors(Exception):
+    """ raised when error is not known. """
+    pass
 
 class NseIndia:
-    """
-    NseIndia : class to get data from National Stocks Exchange of india
-    """
+    """ NseIndia : class to get data from National Stocks Exchange of india. """
     def __init__(self):
         pass
 
-    def bhavcopy(self):
-        '''Fatch bhavcopy
-        return : pandas Dataframe  
+    def bhavcopy(self, date = None):
         '''
-        pass
+        Fatch bhavcopy
+        date : (data-type : str) dd-mm-yyyy formate
+        return : (data-type : pandas Dataframe) 
+        '''
+        if date is None:
+            date = datetime.datetime.now()   # choose today if no date is specified
+        else:
+            try:
+                d, m, y = [int(i) for i in date.split('-')]
+            except:
+                if re.compile(date, '[0-9]+-[0-9]+-[0-9]+'):
+                    raise BadDateFormat("date is not specified in correct format.")
+                else:
+                    raise UnknownError('Cannot find out error.')
