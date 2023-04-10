3fd29a03bf699d565b8e1cb590202cb3e3a07c89
mohitrajrathor <mohitrajrathor@gmail.com>
2023-04-10 23:18:34 +0530

    nseindia class initiated

--- fatch_data.py
+++ fatch_data.py
@@ -1,1 +1,12 @@
 # python module to fatch stocks data from web 
+import requests
+
+class NseIndia:
+    """
+    NseIndia : class to get data from National Stocks Exchange of india
+    """
+    def __init__(self):
+        pass
+
+    def bhavcopy(self):
+        pass
