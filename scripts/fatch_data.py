a3562560af4e4812cbbe7929fe1694b5d92cad21
mohitrajrathor <mohitrajrathor@gmail.com>
2023-04-11 22:38:56 +0530

    add bulk deals function

--- scripts/fatch_data.py
+++ scripts/fatch_data.py
@@ -6,6 +6,21 @@ import datetime
 import re
 
 
+# common funcs. 
+def date_parser(date):
+    if date is None:
+            date = datetime.datetime.now()   # choose today if no date is specified
+            d, m, y = [int(i) for i in (date.strftime('%d-%m-%Y')).split('-')]
+            return (d, m, y)
+        else:
+            try:
+                d, m, y = [int(i) for i in date.split('-')]
+            except:
+                if re.match('[0-9]+-[0-9]+-[0-9]+', date):
+                    raise BadDateFormat("date is not specified in correct format.")
+                else:
+                    raise UnknownError('Cannot find out error.')
+
 # exceptions class.
 class BadDateFormat(Exception):
     """ raised if date is not specified in correct or instructed format. """
@@ -26,21 +41,19 @@ class NseIndia:
     def get_bhavcopy(self, date = None):
         '''
         Fatch bhavcopy
-        date : (data-type : str) dd-mm-yyyy formate
-        return : (data-type : pandas Dataframe) 
+        date : (data-type : str) dd-mm-yyyy format
+        return : data-type : pandas Dataframe 
         '''
-        if date is None:
-            date = datetime.datetime.now()   # choose today if no date is specified
-            d, m, y = [int(i) for i in (date.strftime('%d-%m-%Y')).split('-')]
-        else:
-            try:
-                d, m, y = [int(i) for i in date.split('-')]
-            except:
-                if re.match('[0-9]+-[0-9]+-[0-9]+', date):
-                    raise BadDateFormat("date is not specified in correct format.")
-                else:
-                    raise UnknownError('Cannot find out error.')
+        d, m, y = date_parser(date)
 
+
+    def bulk_deals(self, date = None):
+        '''
+        Fatch bulk deals data
+        date : (data-type : str) dd-mm-yyyy format 
+        return : pd.DataFrame 
+        '''
+        d, m, y = date_parser(date)
 
 if __name__ == "__main__":
     pass
